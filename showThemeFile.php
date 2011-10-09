6d29d244643577b0fd62c69d4735e97154960fa0
asdasDan <asdasDan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-09 20:53:08 +0000

    Doing more effective template loading, using wordpress' template loader.
    git-svn-id: https://plugins.svn.wordpress.org/show-theme-file/trunk@449245 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- showThemeFile.php
+++ showThemeFile.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
   Plugin Name: Show Theme File
   Description: Displays the theme file used to render the current page.
@@ -8,181 +7,50 @@
   Version: 0.0.1
  */
 
-function getThemeFile() {
-    global $wp_query;
-    global $post;
+function getWpTemplate() {
+    if (defined('WP_USE_THEMES') && WP_USE_THEMES) {
+       $template = false;
+       
+       if     (is_404()		   && $template = get_404_template()):
+       elseif (is_search() 	   && $template = get_search_template()):
+       elseif (is_tax()            && $template = get_taxonomy_template()):
+       elseif (is_front_page() 	   && $template = get_front_page_template()):
+       elseif (is_home()           && $template = get_home_template()):
+       elseif (is_attachment() 	   && $template = get_attachment_template()):
+       elseif (is_single()     	   && $template = get_single_template()):
+       elseif (is_page()           && $template = get_page_template()):
+       elseif (is_category() 	   && $template = get_category_template()):
+       elseif (is_tag()      	   && $template = get_tag_template()):
+       elseif (is_author()         && $template = get_author_template()):
+       elseif (is_date()           && $template = get_date_template()):
+       elseif (is_archive()        && $template = get_archive_template()):
+       elseif (is_comments_popup() && $template = get_comments_popup_template()):
+       elseif (is_paged()          && $template = get_paged_template()):       
+       else:
+	$template = get_index_template();
+       endif;
 
-    $themeDir = TEMPLATEPATH;
-    $themeFile = '';
-
-    if (is_home()) {
-        if (file_exists($themeDir . '/' . 'home.php')) {
-            $themeFile = 'home.php';
-        }
-
-        return $themeFile;
+       return str_replace(ABSPATH, '', $template);
+    } else {
+       return null;
     }
-
-    if (is_front_page()) {
-        if (file_exists($themeDir . '/' . 'front-page.php')) {
-            $themeFile = 'front-page.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_404()) {
-        if (file_exists($themeDir . '/' . '404.php')) {
-            $themeFile = '404.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_search()) {
-        if (file_exists($themeDir . '/' . 'search.php')) {
-            $themeFile = 'search.php';
-        }
-
-        return $themeFile;
-    }
-
-
-    if (is_date()) {
-        if (file_exists($themeDir . '/' . 'date.php')) {
-            $themeFile = 'date.php';
-        } elseif (file_exists($themeDir . '/' . 'archive.php')) {
-            $themeFile = 'archive.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_author()) {
-        $authorNiceName = $wp_query->queried_object->user_nicename;
-        $authorID = $wp_query->queried_object->ID;
-
-        if (file_exists($themeDir . '/' . 'author-' . $authorNiceName . '.php')) {
-            $themeFile = 'author-' . $authorNiceName . '.php';
-        } elseif (file_exists($themeDir . '/' . 'author-' . $authorID . '.php')) {
-            $themeFile = 'author-' . $authorID . '.php';
-        } elseif (file_exists($themeDir . '/' . 'author.php')) {
-            $themeFile = 'author.php';
-        } elseif (file_exists($themeDir . '/' . 'archive.php')) {
-            $themeFile = 'archive.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_category()) {
-        $theCategory = get_category(get_query_var('cat'));
-
-        $theSlug = $theCategory->slug;
-        $theID = $theCategory->cat_ID;
-
-        if (file_exists($themeDir . '/' . 'category-' . $theSlug . '.php')) {
-            $themeFile = 'category-' . $theSlug . '.php';
-        } elseif (file_exists($themeDir . '/' . 'category-' . $theID . '.php')) {
-            $themeFile = 'category-' . $theID . '.php';
-        } elseif (file_exists($themeDir . '/' . 'category.php')) {
-            $themeFile = 'category.php';
-        } elseif (file_exists($themeDir . '/' . 'archive.php')) {
-            $themeFile = 'archive.php';
-        }
-
-        return $themeFile;
-    }
-
-
-    if (is_tag()) {
-        $tagInfo = get_query_var('tag_id');
-        $theTag = get_tag($tagInfo);
-
-        $theSlug = $theTag->slug;
-        $theID = $theTag->tag_id;
-
-        if (file_exists($themeDir . '/' . 'tag-' . $theSlug . '.php')) {
-            $themeFile = 'tag-' . $theSlug . '.php';
-        } elseif (file_exists($themeDir . '/' . 'tag-' . $theID . '.php')) {
-            $themeFile = 'tag-' . $theID . '.php';
-        } elseif (file_exists($themeDir . '/' . 'tag.php')) {
-            $themeFile = 'tag.php';
-        } elseif (file_exists($themeDir . '/' . 'tag.php')) {
-            $themeFile = 'archive.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_archive()) {
-        $thePostID = $wp_query->post->ID;
-        $thePostType = get_post_type($thePostID);
-
-        if (file_exists($themeDir . '/' . 'archive-' . $thePostType . '.php')) {
-            $themeFile = 'archive-' . $thePostType . '.php';
-        } elseif (file_exists($themeDir . '/' . 'archive.php')) {
-            $themeFile = 'archive.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_single()) {
-        $thePostID = $wp_query->post->ID;
-        $thePostType = get_post_type($thePostID);
-
-        if (file_exists($themeDir . '/' . 'single-' . $thePostType . '.php')) {
-            $themeFile = 'single-' . $thePostType . '.php';
-        } elseif (file_exists($themeDir . '/' . 'single.php')) {
-            $themeFile = 'single.php';
-        }
-    }
-
-    if (is_attachment()) {
-        $thePostID = $wp_query->post->ID;
-
-        $mimeType = get_post_mime_type($thePostID);
-
-        if (file_exists($themeDir . '/' . $mimeType . '.php')) {
-            $themeFile = $mimeType . '.php';
-        } elseif (file_exists($themeDir . '/' . 'attachment.php')) {
-            $themeFile = 'attachment.php';
-        } elseif (file_exists($themeDir . '/' . 'single.php')) {
-            $themeFile = 'single.php';
-        }
-
-        return $themeFile;
-    }
-
-    if (is_page()) {
-        $thePost = get_post($post->ID, 'OBJECT');
-        $thePostID = $thePost->ID;
-        $thePostSlug = $thePost->post_name;
-
-        $specificThemeFile = get_post_meta($thePostID, '_wp_page_template', true);
-
-        if (($specificThemeFile != 'default') && (file_exists($themeDir . '/' . $specificThemeFile))) {
-            $themeFile = $specificThemeFile;
-        } elseif (file_exists($themeDir . '/' . 'page-' . $thePostSlug . '.php')) {
-            $themeFile = 'page-' . $thePostSlug . '.php';
-        } elseif (file_exists($themeDir . '/' . 'page-' . $thePostID . '.php')) {
-            $themeFile = 'page-' . $thePostID . '.php';
-        } elseif (file_exists($themeDir . '/' . 'page.php')) {
-            $themeFile = 'page.php';
-        }
-
-        return $themeFile;
-    }
-
-    return false;
 }
 
 function outputThemeFilename() {
+    global $stfOptions;
+
     echo '<div id="showtheme" style="position: absolute; z-index: 9999; padding: 1em; background: white; color: black; opacity: 0.8; border: dotted 2px #999;">';
-    echo (getThemeFile()) ? getThemeFile() : 'Unknown theme file';
+    echo (getWpTemplate() !== null) ? getWpTemplate() : 'Unknown theme file';
     echo '</div>';
     return;
 }
 
-add_action('wp_head', 'outputThemeFilename');
+$stfOptions = array('require_admin' => true);
+
+if ($stfOptions['require_admin'] == true) {
+   if (current_user_can('administrator')) {
+     add_action('wp_head', 'outputThemeFilename');
+   }
+} else {
+     add_action('wp_head', 'outputThemeFilename');
+}
